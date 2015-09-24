881fc0eaed157f4f2fb644f19d1cb05e5ca285f1
Matt Deuschle <mdeuschle@gmail.com>
2015-09-23 22:22:36 -0700

    Initial Commit

---
+++ TaxCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TaxCalculator
+//
+//  Created by Matt Deuschle on 9/23/15.
+//  Copyright © 2015 Matt Deuschle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TaxCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TaxCalculator
+//
+//  Created by Matt Deuschle on 9/23/15.
+//  Copyright © 2015 Matt Deuschle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
